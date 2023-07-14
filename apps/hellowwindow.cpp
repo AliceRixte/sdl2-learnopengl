23a76e0e5ba18f848b569f4b4dead8d9b870ad3c
Alice Rixte <alice.rixte@gmail.com>
2023-07-14 12:02:07 +0200

    Hello window !

--- apps/hellowwindow.cpp
+++ apps/hellowwindow.cpp
@@ -1,16 +1,3 @@
-/*
-    Minimal SDL2 + OpenGL3 example.
-    Author: https://github.com/koute
-    This file is in the public domain; you can do whatever you want with it.
-    In case the concept of public domain doesn't exist in your jurisdiction
-    you can also use this code under the terms of Creative Commons CC0 license,
-    either version 1.0 or (at your option) any later version; for details see:
-        http://creativecommons.org/publicdomain/zero/1.0/
-    This software is distributed without any warranty whatsoever.
-    Compile and run with: gcc opengl3_hello.c `sdl2-config --libs --cflags` -lGL -Wall && ./a.out
-*/
-
-//#define GL_GLEXT_PROTOTYPES
 
 #include <SDL2/SDL.h>
 #include <GL/glew.h>
@@ -30,13 +17,17 @@ int main( int argc, char * argv[] )
     SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
 
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 2 );
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK,
+        SDL_GL_CONTEXT_PROFILE_CORE );
 
     static const int width = 800;
     static const int height = 600;
 
-    SDL_Window * window = SDL_CreateWindow( "", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+    SDL_Window * window = SDL_CreateWindow( 
+        "Learn OpenGL with SDL window",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
     SDL_GLContext context = SDL_GL_CreateContext( window );
 
     glewExperimental = GL_TRUE;
@@ -48,11 +39,10 @@ int main( int argc, char * argv[] )
         glViewport(0, 0, width, height);
     }
 
+    bool keepRunning = true;
 
-    for( ;; )
+    while(keepRunning)
     {
-        glClear( GL_COLOR_BUFFER_BIT );
-
         SDL_Event event;
         while( SDL_PollEvent( &event ) )
         {
@@ -60,10 +50,18 @@ int main( int argc, char * argv[] )
             {
                 case SDL_KEYUP:
                     if( event.key.keysym.sym == SDLK_ESCAPE )
-                        return 0;
+                        keepRunning = false;
+                    break;
+                case SDL_WINDOWEVENT:
+                    if( event.window.event == SDL_WINDOWEVENT_CLOSE)
+                        keepRunning = false;
                     break;
             }
         }
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear( GL_COLOR_BUFFER_BIT );
+
 
         SDL_GL_SwapWindow( window );
         SDL_Delay( 1 );
